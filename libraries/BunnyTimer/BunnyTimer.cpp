5e3eef23cb57d4b323775cadcf1017b5d91323ef
Captjack2787 <35874221+Captjack2787@users.noreply.github.com>
2022-02-05 17:16:59 -0600

    Support callback

--- libraries/BunnyTimer/BunnyTimer.cpp
+++ libraries/BunnyTimer/BunnyTimer.cpp
@@ -1,4 +1,4 @@
-#include "BunnyTimer.h"
+#include "BunnyTimer.hpp"
 
 BunnyTimer::BunnyTimer() { }
 
@@ -6,25 +6,36 @@ BunnyTimer::~BunnyTimer() { }
 
 void BunnyTimer::update() {
     _triggered = false;
-    unsigned long t = millis();
-    if(_expire_time >= 0 && t > _expire_time) {
+    unsigned long currentMillis = millis();
+    if(_expire_time >= 0 && currentMillis > _expire_time) {
         _triggered = true;
+        if(_callback != NULL) {
+            _callback();
+        }
         if(_timeout_mode) {
             _expire_time = -1;
         } else {
-            _expire_time = t + _millis;
+            _expire_time = currentMillis + _millis;
         }
     }
 }
 
-void BunnyTimer::setInterval(unsigned long t_millis, bool timeout_mode) {
-    _timeout_mode = timeout_mode;
+void BunnyTimer::setInterval(unsigned long t_millis) {
+    _timeout_mode = false;
     _millis = t_millis;
     _expire_time = millis() + _millis;
 }
 
-void MyTimer::setTimeout(unsigned long t_millis) {
-    setInterval(t_millis, true);
+void BunnyTimer::setCallback(f_int_t callback) {
+    _callback = callback;
+}
+
+
+
+void BunnyTimer::setTimeout(unsigned long t_millis) {
+    _timeout_mode = true;
+    _millis = t_millis;
+    _expire_time = millis() + _millis;
 }
 
 bool BunnyTimer::triggered() {
